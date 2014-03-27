9a5e64c1682abc1e85fe2e36a41565e97193e18b
Wojciech <wojciech@makama.pl>
2014-03-27 22:39:36 +0100

    PTEST.cs

---
+++ PTEST.cs
@@ -0,0 +1,20 @@
+/*
+ * PTEST
+ * http://pl.spoj.com/problems/PTEST/
+ */
+using System;
+
+
+namespace SPOJ
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(System.Console.ReadLine());
+            int b = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
